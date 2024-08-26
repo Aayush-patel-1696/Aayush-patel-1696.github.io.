909279a428bd1e274f07bfc75c5a6205d8c91d35
Aayush-patel-1696 <69187017+Aayush-patel-1696@users.noreply.github.com>
2024-08-26 13:20:51 -0400

    changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -423,7 +423,7 @@ export const skills = [
 
 export const experiences = [
   {
-    organisation: "Analyst (Machine Learning & Data), Risk Analytics Division,Linedata",
+    organisation: "Analyst (Machine Learning & Data), Risk Analytics Division, Linedata",
     logo: linedata,
     link: "https://www.linedata.com/",
     positions: [
@@ -1330,5 +1330,5 @@ export const socialMedia = [
 export const aboutMe = {
     name: "Aayush Patel",
     tagLine: " Aspiring Quant | Rutgers'24 | ex-ML Engineer | IIT Bombay'21",
-    intro: "I strive to solve complex problems at the intersection of finance and engineering through the fusion of models, algorithms, and data. I have 2+ years of experience as a Software Developer and Machine Learning Engineer in the finance industry. My work focuses on but not limited to Stochastic Portfolio Optimization, Derivative Pricing, Quant/Risk Modeling and High Performance Numerical Computing. Presently, I am working on Risk Averse Reinforcement Learning under the supervision of Prof. Andrzej Ruszczynski."
+    intro: "I strive to solve complex problems at the intersection of finance and engineering through the fusion of models, algorithms, and data. I have 2+ years of experience as a Software Developer and Machine Learning Engineer in the finance industry. My work focuses on but not limited to Stochastic Portfolio Optimization, Derivative Pricing, Quant/Risk Modeling and High Performance Numerical Computing."
 }
